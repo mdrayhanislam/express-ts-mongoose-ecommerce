245ec21eb23ea3c77ea0abe7fcec482e54d9f64e
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2024-06-15 00:21:07 +0600

    eslint setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from "express";
-// const express = require('express')
 import cors from "cors";
 const app = express();
 
 app.use(cors());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
