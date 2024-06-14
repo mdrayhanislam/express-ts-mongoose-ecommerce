363ccfe3dac431dcfbbeb05e061d5a72f8941b6a
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2024-06-15 01:08:23 +0600

    interface file create

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
-const app = express();
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (req: Request, res: Response) => {
+  const a = 'Hello World'
+  res.send(a)
+})
 
-export default app;
+export default app
--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,9 +1,9 @@
-import dotenv from "dotenv";
-import Path = require("path");
+import dotenv from 'dotenv'
+import Path = require('path')
 
-dotenv.config({ path: Path.join((process.cwd(), ".env")) });
+dotenv.config({ path: Path.join((process.cwd(), '.env')) })
 
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-};
+}
